4c6e8b5831080ad87b57ee33c4bb744ebbc0749e
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-18 13:28:24 -0600

    axios get call works, appends to dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
@@ -7,12 +7,16 @@ import './App.css';
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
 
-  const fetchList = (
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  const fetchList = ()  => {
     axios
       .get('/list')
       .then(response => setShoppingList(response.data))
       .catch(err => console.log('There was an error getting the list', err))
-  )
+  };
   
   // const 
     return (
@@ -22,6 +26,7 @@ function App() {
                 <p>Under Construction...</p>
                 <ul>
                   {shoppingList.map(item => {
+                    return (
                     <li key={item.id}>
                       <div>{item.name}</div>
                       <div>{item.quantity} {item.unit}</div>
@@ -30,6 +35,7 @@ function App() {
                         <button>Remove</button> 
                       </div>
                     </li>
+                    );
                   })}
                 </ul>
             </main>
