13e23cfafc973f75cac8cef7e6aaa8f023c0a13e
Johnshandsable <Jshands@asu.edu>
2021-02-18 14:10:13 -0600

    updated master

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,11 +8,40 @@ import ShoppingList from '../ShoppingList/ShoppingList';
 
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
+  const [newItemName, setNewItemName] = useState('');
+  const [newQuantity, setNewQuantity] = useState(0);
+  const [newUnit, setNewUnit] = useState('');
+  const [newItem, setNewItem] = useState({});
 
   useEffect(() => {
     fetchList();
   }, []);
 
+  // FUNCTIONS
+  const clearItemInputs = () => {
+    setNewItemName('');
+    setNewQuantity(0);
+    setNewUnit('');
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    // put the item info in newItem
+    setNewItem({
+      item: newItemName,
+      quantity: newQuantity,
+      unit: newUnit
+    });
+
+    // shoppingList function - POST in App.jsx
+    // shoppingList(newItem)
+    
+    // clear the inputs
+    clearItemInputs();
+  }
+
+
+  // ROUTES 
   const fetchList = ()  => {
     axios
       .get('/list')
@@ -27,7 +56,15 @@ function App() {
           <Header />
       
           <main>
-            <ItemInputs />
+            <ItemInputs
+            handleSubmit = {handleSubmit} 
+            newItemName = {newItemName} 
+            setNewItemName = {setNewItemName}
+            newQuantity = {newQuantity}
+            setNewQuantity = {setNewQuantity} 
+            newUnit = {newUnit} 
+            setNewUnit = {setNewUnit}
+            />
             
             <ShoppingList
              shoppingList={shoppingList}
--- src/components/ItemInputs/ItemInputs.jsx
+++ src/components/ItemInputs/ItemInputs.jsx
@@ -1,34 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-function ItemInputs() {
-
-  const [newItemName, setNewItemName] = useState('');
-  const [newQuantity, setNewQuantity] = useState(0);
-  const [newUnit, setNewUnit] = useState('');
-
-  const [newItem, setNewItem] = useState({});
-
-  const handleSubmit =(evt) => {
-    evt.preventDefault();
-    // put the item info in newItem
-    setNewItem({
-      item: newItemName,
-      quantity: newQuantity,
-      unit: newUnit
-    });
-
-    // shoppingList function - POST in App.jsx
-    // shoppingList(newItem)
-    
-    // clear the inputs
-    clearItemInputs();
-  }
-
-  const clearItemInputs = () => {
-    setNewItemName('');
-    setNewQuantity(0);
-    setNewUnit('');
-  }
+function ItemInputs({
+  handleSubmit, 
+  newItemName, 
+  setNewItemName, 
+  newQuantity, 
+  setNewQuantity, 
+  newUnit, 
+  setNewUnit}) {
 
   return (
     <>
