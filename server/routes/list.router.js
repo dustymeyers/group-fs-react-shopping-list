997f5197a7e7436690ab64a7e940b897afb651ce
Johnshandsable <Jshands@asu.edu>
2021-02-18 10:40:58 -0600

    finished PUT route on server-side

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
-// TODO - Add routes here...
 
-
-/** /list 
- * 
- * GET ROUTE at /list 
-*/
+// GET ROUTE
 router.get('/', (req, res) => {
   console.log('GET inside /list')
   const sqlText = 'SELECT * FROM "shopping_list"';
@@ -24,10 +19,30 @@ router.get('/', (req, res) => {
         })
 }) // end GET
 
+// PUT ROUTE
+router.put('/:id', (req, res) => {
+  console.log('PUT inside /list')
+  console.log('req.params.id', req.params.id);
+  
+  const itemId = req.params.id;
+  const sqlText = 'UPDATE "shopping_list" SET "purchased"=TRUE WHERE "id"=$1';
+
+  pool.query(sqlText, [itemId])
+        .then((result) => {
+            console.log(`Updated an item from the shopping_list`, result);
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+}) // end PUT
+
+// DELETE ROUTE
 router.delete('/:id', (req, res) => {
   console.log('DELETE inside /list')
   console.log('req.params.id', req.params.id);
-  
+
   const itemId = req.params.id;
   const sqlText = 'DELETE FROM "shopping_list" WHERE "id"=$1';
 
@@ -40,6 +55,6 @@ router.delete('/:id', (req, res) => {
             console.log(`Error making database query ${sqlText}`, error);
             res.sendStatus(500); // Good server always responds
         })
-})
+}) // end DELETE
 
 module.exports = router;
