f35707f26e8a9258e75ca3a4697408ffc72f9eac
Johnshandsable <Jshands@asu.edu>
2021-02-18 10:31:23 -0600

    added a DELETE route to the server-side

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -25,13 +25,16 @@ router.get('/', (req, res) => {
 }) // end GET
 
 router.delete('/:id', (req, res) => {
-  console.log('GET inside /list')
-  const sqlText = 'SELECT * FROM "shopping_list"';
+  console.log('DELETE inside /list')
+  console.log('req.params.id', req.params.id);
+  
+  const itemId = req.params.id;
+  const sqlText = 'DELETE FROM "shopping_list" WHERE "id"=$1';
 
-  pool.query(sqlText)
+  pool.query(sqlText, [itemId])
         .then((result) => {
-            console.log(`Got back stuff from the database`, result);
-            res.send(result.rows);
+            console.log(`Deleted an item from shopping_list`, result);
+            res.sendStatus(200);
         })
         .catch((error) => {
             console.log(`Error making database query ${sqlText}`, error);
