2e2f10c2d62fb88e848424a2cb5516e58d83b813
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-18 13:04:57 -0600

    built fetch list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const fetchList = (
+    axios
+      .get('/list')
+      .then(response => setShoppingList(response.data))
+      .catch(err => console.log('There was an error getting the list', err))
+  )
+  
   // const 
     return (
         <div className="App">
@@ -12,14 +21,16 @@ function App() {
             <main>
                 <p>Under Construction...</p>
                 <ul>
-                  <li>
-                    <div>Apples</div>
-                    <div>5 lbs</div>
-                    <div>
-                      <button>Buy</button>
-                      <button>Remove</button> 
-                    </div>
-                  </li>
+                  {shoppingList.map(item => {
+                    <li key={item.id}>
+                      <div>{item.name}</div>
+                      <div>{item.quantity} {item.unit}</div>
+                      <div>
+                        <button>Buy</button>
+                        <button>Remove</button> 
+                      </div>
+                    </li>
+                  })}
                 </ul>
             </main>
         </div>
