902f3a253cfcc2c9dd613687ec32fa9b3c4d8317
Johnshandsable <Jshands@asu.edu>
2021-02-19 09:30:12 -0600

    finalized feature-list-item-buttons

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -11,10 +11,12 @@ function ShoppingList({
           <li key={item.id}>
             <div>{item.name}</div>
             <div>{item.quantity} {item.unit}</div>
-            <ListItemButtons 
-            item={item} 
-            updateItemFromList={updateItemFromList}
-            deleteItemFromList={deleteItemFromList}/>
+            {item.purchased 
+            ? <p>Purchased</p> 
+            : <ListItemButtons
+               item={item}
+               updateItemFromList={updateItemFromList}
+               deleteItemFromList={deleteItemFromList}/>}
           </li>
         );
       })}
